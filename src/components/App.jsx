1d5b5f109bc31c10e4a9f76c4c788bd6c4475a9e
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-08-30 19:10:38 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux'; // Импортируем useSelector для доступа к состоянию из Redux
+import { useSelector, useDispatch  } from 'react-redux'; // Импортируем useSelector для доступа к состоянию из Redux
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
+import { setFilter } from '../redux/filtersSlice'; // Импортируем экшен setFilter
 
 const App = () => {
   const filter = useSelector(state => state.contacts.filter); // Получаем значение фильтра из Redux
-
+  const dispatch = useDispatch(); // Получаем функцию dispatch
   return (
 
     <div className="book_section">
@@ -15,7 +16,7 @@ const App = () => {
       <div className="contact_form">
         <h2>Contacts</h2>
         <h3>Find contacts by name</h3>
-        <Filter value={filter} />
+        <Filter value={filter} onChange={value => dispatch(setFilter(value))} />
         <ContactList />
       </div>
     </div>
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux'; // Импортируем useDispatch
+import { setFilter } from '../redux/filtersSlice'; // Импортируем экшен setFilter
 
-const Filter = ({ value, onChange }) => {
+const Filter = ({ value }) => {
+  const dispatch = useDispatch(); // Получаем функцию dispatch
   return (
     <input
       type="text"
       placeholder="Filter contacts"
       value={value}
-      onChange={e => onChange(e.target.value)}
+     onChange={e => dispatch(setFilter(e.target.value))} 
     />
   );
 };
