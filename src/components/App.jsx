16c415f06ab7fef92f0ae65c1282c11bd34aae3e
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-08-30 14:13:07 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux'; // Импортируем useSelector для доступа к состоянию из Redux
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
+// import { useSelector } from 'react-redux'; // Импортируем useSelector для доступа к состоянию из Redux
+// import ContactForm from './ContactForm';
+// import ContactList from './ContactList';
+// import Filter from './Filter';
 
 const App = () => {
-  const filter = useSelector(state => state.contacts.filter); // Получаем значение фильтра из Redux
+  // const filter = useSelector(state => state.contacts.filter); // Получаем значение фильтра из Redux
 
   return (
-    <div className="book_section">
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <div className="contact_form">
-        <h2>Contacts</h2>
-        <h3>Find contacts by name</h3>
-        <Filter value={filter} />
-        <ContactList />
-      </div>
-    </div>
+    <div>HELLO WORLD</div>
+    // <div className="book_section">
+    //   <h1>Phonebook</h1>
+    //   <ContactForm />
+    //   <div className="contact_form">
+    //     <h2>Contacts</h2>
+    //     <h3>Find contacts by name</h3>
+    //     <Filter value={filter} />
+    //     <ContactList />
+    //   </div>
+    // </div>
   );
 };
 
